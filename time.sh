8a629195f4b673c46f198ddf4ab5812190530df6
shivmodi1203 <shivmodi258@gmail.com>
2024-07-17 10:16:16 +0530

    handling edge cases

--- time.sh
+++ time.sh
@@ -1,12 +1,24 @@
-echo "Enter time in hh:mm format"
+echo "Enter time in hh:mm format( e.g. 10:10 ) :"
 read time
 hour=$(echo $time | cut -f1 -d ":")
 minut=$(echo $time | cut -f2 -d ":")
-if [ $hour -gt 24 ] || [ $minut -gt 59 ];
-then
-        echo "Enter valid time"
-        exit 0
-fi
+
+# flag=true
+# len=${#time}
+# for ((i=0;i<len;i++));
+# do
+#         if [ "${time:i:1}" == ":" ] ;
+#         then
+#                 flag=false
+#         fi
+# done
+
+# if [ ${flag} == true ] || [ $hour -gt 24 ] || [ $minut -gt 59 ] || [ "${hour}" == "" ] || [ "${minut}" == "" ] || [ ${#hour} -ne 2 ] || [ ${#minut} -ne 2 ];
+# then
+#         echo "The input format is invalid please enter valid time"
+#         exit 0
+# fi
+
 minut=$(($hour*60 + $minut))
 if [ $minut -ge 0 ] && [ $minut -le 360 ];
 then
